55019c1857fa494d9415f5a404029ae927e5592f
mtszkw <mateusz.kwasniak@pm.me>
2022-06-13 17:20:23 +0200

    Dont fix model version

--- src/register.py
+++ src/register.py
@@ -16,18 +16,19 @@ except neptune.exceptions.NeptuneModelKeyAlreadyExistsError:
 
 
 model_version = neptune.init_model_version(
-    model="MLPROD-REVCLF",
-    project="mtszkw/ml-production",
-    api_token=os.environ["NEPTUNE_API_TOKEN"],
-    version=os.environ["GITHUB_SHA"]
+    model='MLPROD-REVCLF',
+    project='mtszkw/ml-production',
+    api_token=os.environ['NEPTUNE_API_TOKEN'],
 )
-model_version["github_sha"] = os.environ["GITHUB_SHA"]
-model_version["github_ref"] = os.environ["GITHUB_REF"]
-model_version["parameters"].upload('parameters.json')
-model_version["training_code"].upload('src/train.py')
+model_version["github_sha"] = os.environ['GITHUB_SHA']
+model_version["github_ref"] = os.environ['GITHUB_REF']
+model_version["model/parameters"].upload('parameters.json')
+model_version["model/training_code"].upload('src/train.py')
+model_version["model/binary"].upload(os.environ['MODEL_ARCHIVE_OUTPUT_PATH'])
+
 model_version.stop()
 
-# model_version["model/binary"].upload()
+
 
 model.stop()
 
