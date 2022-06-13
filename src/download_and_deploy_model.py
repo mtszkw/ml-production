9a262dfbdc89e35122cc639e4d6c8280f1b8d73d
mtszkw <mateusz.kwasniak@pm.me>
2022-06-13 20:14:50 +0200

    Fix workflow names

--- src/download_and_deploy_model.py
+++ src/download_and_deploy_model.py
@@ -42,33 +42,30 @@ if __name__ == "__main__":
 
     # Get all registered models and find the one with correct GIT_COMMIT
     model_versions_df = model.fetch_model_versions_table().to_pandas()
-    print('All versions:', model_versions_df)
+    # print('All versions:', model_versions_df)
     newest_model = model_versions_df.iloc[0]
-        # model_versions_df["data/git_commit"] == os.environ['GIT_COMMIT']
-    # ]
     print('Newest version:', newest_model)
+    version_id = newest_model["sys/id"]
 
-    for _, model_version in newest_model.iterrows():
-        # Download artifacts for the model
-        version_id = model_version["sys/id"]
-        model_version = neptune.init_model_version(
-            project='mtszkw/ml-production',
-            api_token=os.environ['NEPTUNE_API_TOKEN'],
-            version=version_id
-        )
-        print(f"Downloading model binary to model_{version_id}.tar.gz")
-        model_version["model/binary"].download(f"model_{version_id}.tar.gz")
+    # Download artifacts for the model
+    model_version = neptune.init_model_version(
+        project='mtszkw/ml-production',
+        api_token=os.environ['NEPTUNE_API_TOKEN'],
+        version=version_id
+    )
+    print(f"Downloading model binary to model_{version_id}.tar.gz")
+    model_version["model/binary"].download(f"model_{version_id}.tar.gz")
 
-        # Upload model binary to S3
-        s3_path = export_to_s3(
-            model_archive=f"model_{version_id}.tar.gz",
-            region=os.environ['MODEL_AWS_REGION'],
-            bucket_name=os.environ['MODEL_AWS_BUCKET'])
-        
-        # Deploy SageMaker Endpoint from S3 binary
-        # deploy_sagemaker_endpoint_from_s3(model_s3_path=s3_path)
-        
-        # and officially promote the model to production
-        # model_version.change_stage("production")
+    # Upload model binary to S3
+    s3_path = export_to_s3(
+        model_archive=f"model_{version_id}.tar.gz",
+        region=os.environ['MODEL_AWS_REGION'],
+        bucket_name=os.environ['MODEL_AWS_BUCKET'])
+    
+    # Deploy SageMaker Endpoint from S3 binary
+    # deploy_sagemaker_endpoint_from_s3(model_s3_path=s3_path)
+    
+    # and officially promote the model to production
+    # model_version.change_stage("production")
     
     model.stop()
